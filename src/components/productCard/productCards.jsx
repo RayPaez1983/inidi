170152ca8c165bb2fdc089f3efe9c8a5caa720d7
fredddyrp83 <frp.plata@gmail.com>
2022-08-22 16:39:47 -0500

    add the checkout page functionality

--- src/components/productCard/productCards.jsx
+++ src/components/productCard/productCards.jsx
@@ -1,34 +1,52 @@
-import React from "react";
-import { BsCartPlusFill, BsChevronRight } from "react-icons/bs";
+import React, {useEffect} from "react";
+import { BsFileMinusFill, BsFilePlusFill, BsFillCartDashFill } from "react-icons/bs";
 import "./productCard.scss";
 import { useContext } from "react";
 import { CartContext } from "../../context/cart.context";
-import Button from "../common/button/mainButton";
+import { useNavigate } from "react-router-dom";
+
 
 const ProductCard = () => {
-  const { cartCount, addItemToCart, cartItems } = useContext(CartContext);
-  console.log(cartItems, cartCount, 'smell gooood');
+  let navigate = useNavigate();
+  const { cartCount, addItemToCart, cartItems, removeItemToCart, clearItemFromCart, cartTotal } = useContext(CartContext);
+  console.log(cartTotal, 'the total');
    const incrementItem = (item)=>{
       addItemToCart(item)
    }
+    const decrementItem = (item)=>{
+      removeItemToCart(item)
+   }
+    const deleteItem = (item)=>{
+      clearItemFromCart(item)
+   }
+
+   useEffect(() => {
+    if (cartCount === 0) {
+      navigate("/")
+    }
+   }, [cartCount])
   
   return (
     <div >
-      <section >
-        {cartItems.map((card, index) => (
-          <div className="shop" key={index}>
-            <img src={card.imageUrl} alt="" className="shop_img" width="150" height="150"></img>
-
-            <div>
+      <section className="shop-section">
+        {cartItems.map((cart, index) => (
+          <div className="shop-container" key={index}>
+            <h2 className="shop-container_title">{cart.title}</h2>
+            <img src={cart.imageUrl} alt="" className="shop-container_img" width="150" height="150"/>
+            <div ><BsFileMinusFill onClick={()=>decrementItem(cart)}/><span>{cart.quantity}</span><BsFilePlusFill onClick={()=>incrementItem(cart)}/></div>
+             <div>
               <span className="shop__price">
-                ${card.price}
-                <BsCartPlusFill className="shop__icon" />
+                ${cart.price}
               </span>
             </div>
-            <span>{card.quantity}</span>
-            <div onClick={()=>incrementItem(card)}><BsChevronRight/></div>
+            <div>
+              <span className="shop__removex" onClick={()=>deleteItem(cart)}>
+                <BsFillCartDashFill/>
+              </span>
+            </div>
           </div>
         ))}
+        <span>Total: {cartTotal}</span>
       </section>
     </div>
   );
